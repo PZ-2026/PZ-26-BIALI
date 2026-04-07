8c3948d2ee1d47d3a289b8cda0957a0e6d736e0a
devkow77 <kowalsky429@gmail.com>
2026-04-07 21:34:49 +0200

    Utwórz prosty prototyp panelu uzytkownika

---
+++ app/src/main/java/com/example/gymmanager/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.example.gymmanager.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Green80 = Color(0xFF0BB01B)
+val LightGreen80 = Color(0xFFDCEDC8)
+val Mint80 = Color(0xFFA7FFEB)
---
+++ app/src/main/java/com/example/gymmanager/ui/theme/Theme.kt
@@ -0,0 +1,30 @@
+package com.example.gymmanager.ui.theme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+// Konfiguracja dla trybu jasnego
+private val LightColorScheme = lightColorScheme(
+    primary = Green80,
+    secondary = LightGreen80,
+    tertiary = Green80,
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onBackground = Color.Black,
+    onSurface = Color.Black
+)
+
+@Composable
+fun GymManagerTheme(
+    content: @Composable () -> Unit
+) {
+    val colorScheme = LightColorScheme
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
