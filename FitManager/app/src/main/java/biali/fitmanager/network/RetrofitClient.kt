2c9267ce5d8d383dfd304667debbbc46915dcd54
Olik444 <olaf.slowik@gmail.com>
2026-06-07 17:53:56 +0200

    a

--- FitManager/app/src/main/java/biali/fitmanager/network/RetrofitClient.kt
+++ FitManager/app/src/main/java/biali/fitmanager/network/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.0.241:8080/"
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
     private val okHttpClient: OkHttpClient by lazy {
         val logging = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
